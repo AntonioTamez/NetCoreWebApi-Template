0ae13b9b4d71e2d728f96828f200ad1009ef17f0
AntonioTamez <antonio.tamez@hotmail.com>
2025-04-02 22:37:33 -0600

    repository created

---
+++ NetCoreWebApi-Template/Repositories/Interfaces/IUsersRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NetCoreWebApi_Template.Shared.DTOs;
+using NetCoreWebApi_Template.Shared.Entities;
+using NetCoreWebApi_Template.Shared.Responses;
+
+namespace NetCoreWebApi_Template.Repositories.Interfaces
+{
+    public interface IUsersRepository
+    {
+        Task<ActionResponse<IEnumerable<User>>> GetAsync(PaginationDTO pagination);
+
+        Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
+        Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+        Task<User> GetUserAsync(string email);
+
+        Task<User> GetUserAsync(Guid userId);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+
+        Task<SignInResult> LoginAsync(LoginDTO model);
+
+        Task LogoutAsync();
+
+    }
+
+}
